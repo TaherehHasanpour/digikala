61d5ffadbd24187d5a2e41eedff3f0d2ff470772
Hassanpour <taherehassanpuor80@gmail.com>
2024-07-27 01:47:36 -0700

    Running all parts of the application before Android Studio update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,72 +11,72 @@ val apiKeyPropertiesFile = rootProject.file("key.properties")
 val apiKeyProperties = Properties()
 apiKeyProperties.load(apiKeyPropertiesFile.inputStream())
 
-        android {
-            namespace = "com.example.digikala"
-            compileSdk = 34
+android {
+    namespace = "com.example.digikala"
+    compileSdk = 34
 
-            defaultConfig {
-                applicationId = "com.example.digikala"
-                minSdk = 24
-                targetSdk = 34
-                versionCode = 1
-                versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.example.digikala"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-                buildConfigField("String", "X_API_KEY", apiKeyProperties.getProperty("X_API_KEY"))
-                buildConfigField("String", "KEY", apiKeyProperties.getProperty("KEY"))
-                buildConfigField("String", "IV", apiKeyProperties.getProperty("IV"))
+        buildConfigField("String", "X_API_KEY", apiKeyProperties.getProperty("X_API_KEY"))
+        buildConfigField("String", "KEY", apiKeyProperties.getProperty("KEY"))
+        buildConfigField("String", "IV", apiKeyProperties.getProperty("IV"))
 
-                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                vectorDrawables {
-                    useSupportLibrary = true
-                }
-            }
-
-            buildTypes {
-                release {
-                    isMinifyEnabled = false
-                    proguardFiles(
-                        getDefaultProguardFile("proguard-android-optimize.txt"),
-                        "proguard-rules.pro"
-                    )
-                }
-            }
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-            buildFeatures {
-                compose = true
-                buildConfig = true
-            }
-            composeOptions {
-                kotlinCompilerExtensionVersion = "1.4.4"
-            }
-            packaging {
-                resources {
-                    excludes += "/META-INF/{AL2.0,LGPL2.1}"
-                }
-            }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
         }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.ui:ui-text-android:1.5.4")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.ui:ui-text-android:1.6.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -103,7 +103,7 @@ dependencies {
     //hilt
     implementation("com.google.dagger:hilt-android:${System.getProperty("hilt_version")}")
     kapt("com.google.dagger:hilt-android-compiler:${System.getProperty("hilt_version")}")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     //compose navigation
     implementation("androidx.navigation:navigation-compose:${System.getProperty("nav_version")}")
@@ -125,15 +125,14 @@ dependencies {
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
 
     //Accompanist-Pager
-    implementation ("com.google.accompanist:accompanist-pager:0.29.0-alpha")
-    implementation ("com.google.accompanist:accompanist-pager-indicators:0.29.0-alpha")
+    implementation("com.google.accompanist:accompanist-pager:0.29.0-alpha")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.29.0-alpha")
 
     //zarinpal
-    implementation ("com.zarinpal:payment-provider-ktx:0.5.3")
+    implementation("com.zarinpal:payment-provider-ktx:0.5.3")
 
     //paging
-    implementation ("androidx.paging:paging-compose:1.0.0-alpha10")
-
+    implementation("androidx.paging:paging-compose:1.0.0-alpha18")
 
 
 }
